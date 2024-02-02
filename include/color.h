8817982b6eefb9fece4113c7b1fe3c751860bad5
Esteban Rigolon <erigolon@c3r10s5.42malaga.com>
2024-02-02 17:01:09 +0100

    quitando colores

--- include/color.h
+++ include/color.h
@@ -17,6 +17,6 @@
 # define CYAN_BOLD    	"\033[1;36m"
 # define BLUE_BOLD    	"\033[1;34m"
 
-# define READLINE_MSG	"\033[1;36mminishell\033[34m$ \033[0m"
+# define READLINE_MSG	"minishell$ "
 
 #endif
